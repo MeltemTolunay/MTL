5fac57d4d69ffbb3085793b7605eb9c071e398e1
E. Meltem Tolunay <34979291+MeltemTolunay@users.noreply.github.com>
2018-05-07 16:49:48 -0700

    Update jaccard.py

--- jaccard.py
+++ jaccard.py
@@ -1,6 +1,7 @@
 import os
 import scipy.io
 import numpy as np
+import operator
 """
 Python script to compute Jaccard similarities between gender and all others 
 """
@@ -19,13 +20,22 @@ directory = os.fsencode(labels_dir)
 for i, file in enumerate(os.listdir(directory)):
     dir = os.path.join(directory, file)
     if categories[i] != 'sleevelength' and categories[i] != 'neckline' and categories[i] != 'category':
-        attributes[categories[i]] = scipy.io.loadmat(dir)['GT'] - 1  # (1856, 1)
+        labels = scipy.io.loadmat(dir)['GT'] - 1  # (1856, 1)
+        condition = (labels != (0 or 1))
+        labels[condition] = 0
+        attributes[categories[i]] = labels
 
 gender = attributes['gender']
 
 for attribute in attributes:
-    diff = attributes[attribute] - gender
-    intersection = len(diff) - np.count_nonzero(diff)
-    jaccard[attribute] = intersection / len(diff)
+    if attribute != 'gender':
+        diff = attributes[attribute] - gender
+        intersection = len(diff) - np.count_nonzero(diff)
+        jaccard[attribute] = intersection / len(diff)
 
-print(jaccard)
+print(max(jaccard.items(), key=operator.itemgetter(1)))
+
+"""
+Result: Maximum Jaccard similarity is between gender and skin exposure:
+('skin_exposure', 0.5360991379310345)
+"""
